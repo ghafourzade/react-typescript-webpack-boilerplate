3ca247f7fdd2e7d4cdefb142741b12dc9590bc40
Amir <ghafourzadea@gmail.com>
2023-02-12 22:34:37 +0330

    config babel loader with typescript .

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,19 +1,30 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 require("dotenv").config();
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: path.resolve(__dirname, "..", "src", "index.tsx"),
   devServer: {
     port: process.env.PORT,
   },
-  module: {
-    rules: [],
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", "*"],
+    plugins: [new TsconfigPathsPlugin({})],
   },
-  plugins: [new HtmlWebpackPlugin({ template: path.join(__dirname, "/src/index.html") })],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: path.join(__dirname, "..", "src", "index.html") })],
   output: {
     clean: true,
-    path: path.join(__dirname, "/dist"),
+    path: path.join(__dirname, "..", "dist"),
     filename: "bundle.js",
   },
 };
